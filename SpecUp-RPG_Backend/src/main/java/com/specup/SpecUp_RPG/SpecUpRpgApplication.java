49237d129e2a706de02d501ac6f55644125a6fb2
rudals5215 <rudals5215@naver.com>
2026-06-15 00:38:36 +0900

    feat: Next.js 프론트엔드 초기가동 및 1일차 RPG 대시보드 UI 구현

---
+++ SpecUp-RPG_Backend/src/main/java/com/specup/SpecUp_RPG/SpecUpRpgApplication.java
@@ -0,0 +1,13 @@
+package com.specup.SpecUp_RPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecUpRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecUpRpgApplication.class, args);
+	}
+
+}
---
+++ SpecUp-RPG_Backend/src/test/java/com/specup/SpecUp_RPG/SpecUpRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specup.SpecUp_RPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecUpRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
